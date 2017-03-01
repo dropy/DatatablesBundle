a39ecb34c192db0d8a98d70ab1d64cd187fd4a47
Jeremy Fabre <jeremy1fabre@gmail.com>
2017-03-01 09:57:49 +0100

    no message

--- Resources/public/js/childRowColumn.js
+++ Resources/public/js/childRowColumn.js
@@ -24,11 +24,11 @@ $(document).ready(function () {
                 url: url,
                 type: 'GET',
                 dataType: 'json',
-                success: function(d){ //this is specific to my application since I'm adding a child table.
-                    var html = parseDeclinationObject(d);
+                success: function(d){
+                    //Générer un callback JS ou récupération direct en template
                     tr.classList.add('shown');
                     tr.classList.add('opened');
-                    $( '<tr class="child-row"><td></td><td colspan="'+(tr.children.length-2)+'">'+html+'</td><td></td></tr>' ).insertAfter(tr);
+                    $( '<tr class="child-row"><td></td><td colspan="'+(tr.children.length-2)+'">'+d.html+'</td><td></td></tr>' ).insertAfter(tr);
                 }
             });
         }
@@ -44,21 +44,6 @@ $(document).ready(function () {
             //detailOpened[i].parentNode.removeChild(detailOpened[i].nextSibling);
             detailOpened[i].querySelector('.details-control i').classList.remove('glyphicon-minus-sign');
             detailOpened[i].querySelector('.details-control i').classList.add('glyphicon-plus-sign');
-        }
-    }
-
-    function parseDeclinationObject(pDeclinations){
-        if(pDeclinations.length > 0){
-            var html = '<table class="table table-striped table-bordered table-hover"><tr><th>Id</th><th>Titre</th><th>SKU</th><th>Attributs</th></tr>';
-            for(var i=0; i < pDeclinations.length; i++){
-                declination = pDeclinations[i];
-                html+='<tr><td>'+declination.id+'</td><td>'+declination.name+'</td><td>'+declination.sku+'</td><td>empty</td></tr>';
-
-            }
-            html+='</table>';
-            return html;
-        }else{
-            return '<p>Aucune déclinaison pour ce parent</p>'
         }
     }
 });
