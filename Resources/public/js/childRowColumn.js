a6daece6b3255a5d2db8e31cc0fabeea45574fcb
Jeremy Fabre <jeremy1fabre@gmail.com>
2017-03-08 10:20:39 +0100

    Correction event

--- Resources/public/js/childRowColumn.js
+++ Resources/public/js/childRowColumn.js
@@ -1,44 +1,52 @@
 $(document).ready(function () {
 
     var detailRows = [];
+    var datatableId = $('tbody').parent().attr('id');
 
-    $('tbody').click('td.details-control', function(e){
-        var tr = e.target.closest('tr');
-        var url = tr.querySelector('.details-control i').getAttribute('data-content_route');
+    $('#'+datatableId).on('processing.dt', function () {
+        handledetailsChildRow();
+    } );
 
-        if(tr.classList.contains('opened')){
+    function handledetailsChildRow() {
+        $('td.details-control').on('click', function (e) {
+            console.log("la");
+            var tr = e.target.closest('tr');
+            var url = tr.querySelector('.details-control i').getAttribute('data-content_route');
+
+            if (tr.classList.contains('opened')) {
+                hideOpenedDetailRow();
+                return;
+            }
             hideOpenedDetailRow();
-            return;
-        }
-        hideOpenedDetailRow();
 
-        var detailIcon = tr.querySelector('.details-control i');
-        detailIcon.classList.remove('glyphicon-plus-sign');
-        detailIcon.classList.add('glyphicon-minus-sign');
+            var detailIcon = tr.querySelector('.details-control i');
+            detailIcon.classList.remove('glyphicon-plus-sign');
+            detailIcon.classList.add('glyphicon-minus-sign');
 
-        if(tr.classList.contains('shown')){
-            tr.classList.add('opened');
-            tr.nextSibling.style['display'] = 'table-row';
-        }else{
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'json',
-                success: function(d){
-                    //Générer un callback JS ou récupération direct en template
-                    tr.classList.add('shown');
-                    tr.classList.add('opened');
-                    $( '<tr class="child-row"><td></td><td colspan="'+(tr.children.length-2)+'">'+d.html+'</td><td></td></tr>' ).insertAfter(tr);
-                }
-            });
-        }
+            if (tr.classList.contains('shown')) {
+                tr.classList.add('opened');
+                tr.nextSibling.style['display'] = 'table-row';
+            } else {
+                $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (d) {
+                        //Générer un callback JS ou récupération direct en template
+                        tr.classList.add('shown');
+                        tr.classList.add('opened');
+                        $('<tr class="' + tr.classList[0] + ' child-row"><td></td><td colspan="' + (tr.children.length - 2) + '">' + d.html + '</td><td></td></tr>').insertAfter(tr);
+                    }
+                });
+            }
 
 
-    });
+        });
+    }
 
-    function hideOpenedDetailRow(){
+    function hideOpenedDetailRow() {
         var detailOpened = document.querySelectorAll("tr[role='row'].opened");
-        for(var i=0; i < detailOpened.length; i++){
+        for (var i = 0; i < detailOpened.length; i++) {
             detailOpened[i].classList.remove('opened');
             detailOpened[i].nextSibling.style['display'] = 'none';
             //detailOpened[i].parentNode.removeChild(detailOpened[i].nextSibling);
